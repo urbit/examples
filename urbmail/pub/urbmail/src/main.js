f052a16289345b04541b4c929d6c7647406b1313
Galen Wolfe-Pauly <mailroom@office-for.com>
2015-04-12 19:08:27 -0700

    urbmail styles

--- urbmail/pub/urbmail/src/main.js
+++ urbmail/pub/urbmail/src/main.js
@@ -1,89 +1,91 @@
-recl = React.createClass
-div = React.DOM.div
-input = React.DOM.input
+recl   = React.createClass
+div    = React.DOM.div
+input  = React.DOM.input
 button = React.DOM.button
 textar = React.DOM.textarea
 
 Page = recl({
   displayName: 'Page',
+
   render: function(){
-    return  div({}, MessForms({}), Inbox({messages:this.props.messages}))
+    return  div({}, Compose({}), Inbox({messages:this.props.messages}))
   }
 })
 
-MessForms = recl({
+Compose = recl({
   displayName: 'Mess',
-  render: function(){
-    return(
-      div({},
-        To({}),
-        Subj({}),
-        Body({}),
-        button({onClick:this.handleClick}, "Send")
-      )
-    )
+
+  validate: function(to,subj,body){
+    valid = true
+    if(to[0] !== "~" || to.length < 4)
+      valid = "to"
+    if(subj.length < 1)
+      valid = "subj"
+    if(body.length < 1)
+      valid = "body"
+    return valid
   },
+
   handleClick: function(){
-    //if all are valid
+    $to = $(this.getDOMNode()).find('.to')
+    $subj = $(this.getDOMNode()).find('.subj')
+    $body = $(this.getDOMNode()).find('.body')
+
+    if(this.validate($to.val(),$subj.val(),$body.val()) !== true) {
+      $(this.getDOMNode()).find('.'+valid).focus()
+      return false
+    }
+
     urb.send({
       appl:"urbmail",
       data: {
-        to:   $(this.getDOMNode()).find('.to').val(),
-        subj: $(this.getDOMNode()).find('.subj').val(),
-        body: $(this.getDOMNode()).find('.body').val()
+        to:   $to.val(),
+        subj: $subj.val(),
+        body: $body.val()
       }
     }),
-    $(this.getDOMNode()).find('.to').val(''),
-    $(this.getDOMNode()).find('.subj').val(''),
-    $(this.getDOMNode()).find('.body').val('')
-  }
-})
+    $to.val('')
+    $subj.val('')
+    $body.val('')
+  },
 
-To = recl({
   render: function(){
-    return div({}, input({className:"to", placeholder: "To"}))
-  }
-})
-
-Subj = recl({
-  render: function(){
-    return div({}, input({className:"subj",placeholder: "Subject"}))
-  }
-})
-
-Body = recl({
-  render: function(){
-    return div({}, textar({className:"body",placeholder: "Body"}))
+    return(
+      div({},
+        div({}, input({className:"to", placeholder: "To"})),
+        div({}, input({className:"subj",placeholder: "Subject"})),
+        div({}, textar({className:"body",placeholder: "Body"})),
+        button({onClick:this.handleClick}, "Send")
+      )
+    )
   }
 })
 
 Inbox = recl({
   displayName: 'Inbox',
+
   render: function(){
-   msgList = []
+   messages = []
    for (i in this.props.messages){
-     msgList.push(MessageDisp({message: this.props.messages[i]}))
+     messages.push(Message({message:this.props.messages[i]}))
    }
-    return div({}, msgList)
+    return div({}, messages)
   }
 })
 
-MessageDisp = recl({
+Message = recl({
   render: function(){
-    return  div({},
-      div({}, this.props.message.to),
-      div({}, this.props.message.subj),
-      div({}, this.props.message.body))
+    return  div({className:"Message"},
+      div({className:"to"}, this.props.message.to),
+      div({className:"subj"}, this.props.message.subj),
+      div({className:"body"}, this.props.message.body))
   }
 })
 
 
 $(document).ready(function(){
-  mounted = React.render(
-    Page({}), $("#container")[0]
-    )
+  mounted = React.render(Page({}), $("#container")[0])
   urb.bind("/", {appl:'urbmail'}, function(e,d){
-    console.log(e,d.data)
     mounted.setProps({messages:d.data})
   })
 })
